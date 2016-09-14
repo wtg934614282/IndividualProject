4f18b382bdae5962f5338a23c6a49f4114197cc7
illaclv <208006838@qq.com>
2016-09-14 09:39:18 +0800

    test
    test

--- 华大/华大/AppDelegate/AppDelegate.m
+++ 华大/华大/AppDelegate/AppDelegate.m
@@ -16,10 +16,7 @@
 
 @implementation AppDelegate
 
--(void)hahah
-{
-    
-}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];   //设置通用背景颜色
